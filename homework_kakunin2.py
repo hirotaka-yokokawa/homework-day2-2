613a11cf3e688601bb8544551994ab0f5ee62873
Hirotaka <yokokawa.491992@gmail.com>
2018-11-19 11:46:45 +0900

    条件の書き間違いによって結果がFalseになることがあるから注意｡->条件部分はコピペで数値いじったほうがミスが少なくなる｡エラーの場所を教えてくれるPycharmは神｡

--- homework_kakunin2.py
+++ homework_kakunin2.py
@@ -2,11 +2,30 @@ def warikan (amount, number_of_people):
     quotient = amount // number_of_people
     remainder = amount % number_of_people
 
-    return f"1人あたり: {quotient}円, 端数:{remainder}円"
+    return f"1人あたり: {quotient}円, 端数: {remainder}円"
 
 
 def check_warikan():
-    parameters = [(1500, 3 "1人あたり: 500円, 端数: 0円"), (2000, 3 "1人あたり: 666円, 端数: 2円"),]
+    parameters = [
+        (1500, 3, "1人あたり: 500円, 端数: 0円"),
+        (2000, 3, "1人あたり: 666円, 端数: 2円"),
+    ]
+
 
 
     for p in parameters:
+        amount = p[0]    #0は1500､2000
+        number_of_people = p[1]
+        expected = p[2]
+
+
+        result = expected == warikan(amount, number_of_people)
+
+
+        if result:
+            print("OK")
+        else:
+            print("なにかがおかしいよん")
+
+
+check_warikan()
