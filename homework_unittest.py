fec426aa31e6c50ee46150465090c5f96037890e
Hirotaka <yokokawa.491992@gmail.com>
2018-11-19 16:58:26 +0900

    assertEquals(a, b) -> aとbが等しいを表す｡

--- homework_unittest.py
+++ homework_unittest.py
@@ -1,5 +1,3 @@
-# test_warikan.py
-
 import unittest
 
 
@@ -12,19 +10,17 @@ def warikan(amount, number_of_people):
 
 class TestWarikan(unittest.TestCase):
     def test_割り切れるパターン(self):
-        actual = warikan(amount = 1500, number_of_people = 3)
-
+        actual = warikan(amount=1500, number_of_people=3)
 
         expected = "1人あたり: 500円, 端数: 0円"
-        self.assertEquals(expected, actual)
+        self.assertEqual(expected, actual)
 
-    def tesut_端数が出るパターン(self):
-        actual = warikan(amount = 2000, number_of_people = 3)
+    def test_端数がでるパターン(self):
+        actual = warikan(amount=2000, number_of_people=3)
 
         expected = "1人あたり: 666円, 端数: 2円"
-        self.assertEquals(expected, actual)
+        self.assertEqual(expected, actual)
 
 
 if __name__ == "__main__":
     unittest.main()
-
