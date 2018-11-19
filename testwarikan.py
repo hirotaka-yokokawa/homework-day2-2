02ca63c5b43f7ee4e2aa36c98a02133f019dded6
Hirotaka <yokokawa.491992@gmail.com>
2018-11-19 18:07:33 +0900

    frist.commit

--- testwarikan.py
+++ testwarikan.py
@@ -10,7 +10,7 @@ class TestWarikan(unittest.TestCase):
         expected = "1人あたり: 500円, 端数: 0円"
         self.assertEqual(expected, actual)
 
-    def test_端数が出るパターン(self):
+    def test_端数がでるパターン(self):
         actual = warikan(amount=2000, number_of_people=3)
 
         expected = "1人あたり: 666円, 端数: 2円"
