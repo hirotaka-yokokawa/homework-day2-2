7aec27d6b4ab6e5b19b76f276bcd09bd057a2781
Hirotaka <yokokawa.491992@gmail.com>
2018-11-19 18:16:42 +0900

    テスト作ると変更の際にいちいtプリントしなくて良いから楽｡他のソース書くときにも使いたい｡

--- testwarikan.py
+++ testwarikan.py
@@ -5,16 +5,10 @@ from warikan import warikan
 
 class TestWarikan(unittest.TestCase):
     def test_割り切れるパターン(self):
-        actual = warikan(amount=1500, number_of_people=3)
-
-        expected = "1人あたり: 500円, 端数: 0円"
-        self.assertEqual(expected, actual)
+        self.assertEqual("1人あたり: 500円, 端数: 0円", warikan(amount=1500, number_of_people=3))
 
     def test_端数がでるパターン(self):
-        actual = warikan(amount=2000, number_of_people=3)
-
-        expected = "1人あたり: 666円, 端数: 2円"
-        self.assertEqual(expected, actual)
+        self.assertEqual("1人あたり: 666円, 端数: 2円", warikan(amount=2000, number_of_people=3))
 
 
 if __name__ == "__main__":
--- warikan.py
+++ warikan.py
@@ -1,8 +1,4 @@
 def warikan(amount, number_of_people):
-    quotient = amount // number_of_people
-    remainder = amount % number_of_people
+    payment, franction = divmod(amount, number_of_people)
 
-    return f"1人あたり: {quotient}円, 端数: {remainder}円"
-
-
-
+    return f"1人あたり: {payment}円, 端数: {franction}円"
