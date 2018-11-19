c9ab489072f0273f4a5e9ab5da69ce26315c4512
Hirotaka <yokokawa.491992@gmail.com>
2018-11-19 09:17:40 +0900

    second commit コードの意味を日本語で理解できてきた｡オーストラリアに留学したときの頭の使い方､使われ方に酷似している｡楽しい｡

--- homework_day2.py
+++ homework_day2.py
@@ -1,13 +1,30 @@
 def hello():
     print("hello world")
 
+
 def say_hello(name):
     print(f"Hi {name}")
+
 
 say_hello("Bob")
 say_hello("Tom")
 
+
 def double(number):
     return 2 * number
 
+
+print(double(1))
+print(double(2))
+print(double(3))
+
+
+def warikan(amount, number_of_people):
+    quotient = amount // number_of_people
+    remainder = amount % number_of_people
+
+    return f"1人あたり: {quotient}円, 端数{remainder}円"
+
+
+print(warikan(amount=200, number_of_people=3))
 
---
+++ warikan_bring.py
@@ -0,0 +1,5 @@
+
+from homework_day2 import warikan
+
+print(warikan(amount = 5000, number_of_people =  20))
+
